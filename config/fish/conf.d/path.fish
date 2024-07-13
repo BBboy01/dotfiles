f665991c628c496bfb49757cf739916f757ce04c
BBBoy01 <1156678002@qq.com>
2024-07-13 19:26:24 +0800

    chore: add envs prioirty user installed ruby

--- config/fish/conf.d/path.fish
+++ config/fish/conf.d/path.fish
@@ -1,4 +1,5 @@
 fish_add_path -gP $HOMEBREW_PREFIX/bin $HOMEBREW_PREFIX/sbin
-fish_add_path "$CARGO_INSTALL/bin"
-fish_add_path "$BUN_INSTALL/bin"
-fish_add_path "$PNPM_HOME"
+fish_add_path $CARGO_INSTALL/bin
+fish_add_path $BUN_INSTALL/bin
+fish_add_path $PNPM_HOME
+fish_add_path $HOMEBREW_PREFIX/opt/ruby/bin
