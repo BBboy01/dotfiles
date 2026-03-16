6d150e075298abcbff49ea0d66e5036d27887619
BBboy01 <1156678002@qq.com>
2026-03-16 11:04:56 +0800

    fix: prepend fish bin to PATH

--- config/fish/conf.d/path.fish
+++ config/fish/conf.d/path.fish
@@ -1,4 +1,4 @@
-fish_add_path -gP $HOMEBREW_PREFIX/bin $HOMEBREW_PREFIX/sbin
+fish_add_path $HOMEBREW_PREFIX/bin $HOMEBREW_PREFIX/sbin
 fish_add_path $CARGO_HOME/bin
 fish_add_path $BUN_INSTALL/bin
 fish_add_path $PNPM_HOME
